732343aa513c9e7f2f831859d2751476cd92036a
surbhijain87 <surbhijain87@gmail.com>
2020-08-08 16:13:25 +0530

    Initial commit

---
+++ src/test/java/com/fiscom/fis/casestudy/configserver/casestudy/zool/SampleConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiscom.fis.casestudy.configserver.casestudy.zool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
